da0f234867a6665f37f589be6508cd56976db89d
moasvennesson <62596297+moasvennesson@users.noreply.github.com>
2021-05-06 09:10:02 +0200

    Fortfarande fel test

--- tests/stack.test.js
+++ tests/stack.test.js
@@ -19,5 +19,5 @@ test('peek on stack with two or more elements returns the top element', () => {
 });
 
 test('pop should have a str', () => {
-  expect(stack.pop()).tounBeDefined();
+  expect(stack.pop()).toBeUnDefined();
 });
