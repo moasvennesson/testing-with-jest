07f6336add2da744c7aabb93dcff99815af714c3
moasvennesson <62596297+moasvennesson@users.noreply.github.com>
2021-05-06 09:09:09 +0200

    Fel test

--- tests/stack.test.js
+++ tests/stack.test.js
@@ -17,3 +17,7 @@ test('peek on stack with two or more elements returns the top element', () => {
     expect(stack.peek()).toBeDefined();
     expect(stack.peek()).toBe(42);
 });
+
+test('pop should have a str', () => {
+  expect(stack.pop()).tounBeDefined();
+});
